7d11f5f0e1e6f27d020f4d9f9655af8310aa7086
Hexxy <rusakova.nastasia@icloud.com>
2019-06-11 22:20:14 +0300

    Parsed external command, path environmental. Created a functinality to test whether the command exists in path

--- main.c
+++ main.c
@@ -27,6 +27,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 
 /************************** Static Data Definitions **************************/
@@ -60,7 +62,13 @@ int main(int argc, char *argv[]){
     ssize_t result = 0;
     T_Line  inputLine = {0, NULL};
     T_SHELL_COMMANDS currentCommand = NOACT;
-    char* originalInput = NULL;
+    char* externalCommand = NULL;
+    char** params = NULL;
+    char* env_p = NULL;
+    char* env_path = NULL;
+    char* path = NULL;
+    int access_res = 0;
+    bool isExec = false;
 
     if((0 < argc) && (NULL != argv)) {
 
@@ -80,27 +88,68 @@ int main(int argc, char *argv[]){
                 }//for
 
                 //If the command not found in the table - this mean that it's not a built-in command
-                //Parse the command
-                while(NULL != strsep(&inputLine.line, " ")) {
-                    printf("%s\n", inputLine.line);
-                }//while
 
-                //Go through the PATH and check that the command exists via https://www.opennet.ru/man.shtml?topic=access&category=2&russian=0
+                if(NOACT == currentCommand) {
+                    //Parse the command
+                    externalCommand = strsep(&inputLine.line, " ");
+                    
+                    printf("External command: %s\n", externalCommand);                
+                    printf("Params: %s", inputLine.line);
 
-                //Fork a new process and start the command
+                    //Go through the PATH and check that the command exists via https://www.opennet.ru/man.shtml?topic=access&category=2&russian=0
+                    env_p = getenv("PATH");
+                    
 
-                printf(SHELL_STARTUP);
+                    env_path = (char*) malloc(strlen(env_p));
+                    if(NULL != env_path) {
+                        memcpy(env_path, env_p, strlen(env_p));
+                        printf("PATH: %s\n", env_path);
+                        
+                        path = strsep(&env_path, ":");
+                        while(path != env_path) {
+                            path = (char*) realloc(path, strlen(path) + strlen(externalCommand)+1);
+                            strcat(path, "/");
+                            strcat(path, externalCommand);
+                            printf("Checking %s\n", path);
+                            access_res = access(path, X_OK);
+                            if(-1 != access_res) {
+                                isExec = true;
+                                break;
+                            } else {
+                                printf("access returns an error %d\n", access_res);                                
+                            }
+                            free(path);
+                            path = strsep(&env_path, ":");
+                        }//while
+                    }//if
 
-                switch(currentCommand) {
+                    //Fork a new process and start the command
+
+
+                    //Freeing all used memory
+                    if(NULL != externalCommand) {
+                        free(externalCommand);
+                    }//if                    
+
+                } else {
+                    switch(currentCommand) {
                     case EXIT: {
                         exit(0);
                         break;
                     }
-                }//switch                
+                }//switch  
+                }//if
+                
+
+                printf(SHELL_STARTUP);
+
+                              
 
             }//if
 
         }//while
+
+        free(inputLine.line);
 
     }//if
 
