ecc9a4d709666223d0806bef5ab31412e366ce26
Hexxy <rusakova.nastasia@icloud.com>
2019-06-23 19:44:10 +0300

    updated makefile to build tests

--- Makefile
+++ Makefile
@@ -1,9 +1,32 @@
-objects = ./src/main.o
+PROJECT = wish
+BUILD   = build 
+SRC     = src
+T_TEST    = tests 
+CFLAGS  = -c -Wall -I./include
 
-wish : $(objects)
-	cc -o wish $(objects)
+OBJS = $(SRC)/main.o \
+		$(SRC)/shell.o \
+		$(SRC)/utils.o	
 
-main.o : ./src/defs.h
+TEST_OBJS = ./tests/test_shell.o \
+			./tests/test_util.o \
+			
+
+all: wish test 
+
+wish : $(OBJS)
+	cc -o wish $(OBJS)
+
+test : $(TEST_OBJS)
+	cc -o ./tests/test_wish $(TEST_OBJS)
+
+
+main.o : $(SRC)/defs.h
+shell.o: $(SRC)/shell.h
+utils.o: $(SRC)/utils.h
+
+test_shell.o: ./tests/test_shell.h
 
 clean : 
-		rm wish $(objects)
+	rm wish $(OBJS)
+	rm ./tests/test_wish $(TEST_OBJS)
---
+++ tests/test_util.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *
+ *			Shell Test Project
+ *
+ ******************************************************************************
+ *
+ * File :   test_shell.c
+ * 
+ * Author : A Rusakova
+ *
+ * Date :   22.06.19
+ *
+ * History of modifications
+ * Date       Rev.    Reason
+ *
+ ******************************************************************************
+ * FILE DESCRIPTION *//** @file
+ *	
+ * Simple API for shell
+ *
+ *****************************************************************************/
+#include "test_shell.h"
+
+
+
+TEST example_test_case(void) {
+    int r = 0;
+    ASSERT(1 == 1);
+
+    r = rand() % 10;
+    if (r == 1) SKIP();
+    ASSERT(r >= 1);
+    PASS();
+}
+
+TEST expect_equal(void) {
+    int i = 9;
+    ASSERT_EQ(10, i);
+    PASS();
+}
+
+SUITE(utils) {
+    RUN_TEST(example_test_case);
+    RUN_TEST(expect_equal);
+}
